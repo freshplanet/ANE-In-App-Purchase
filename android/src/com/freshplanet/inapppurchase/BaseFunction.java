5f66fec745f2b94b73c31e5208dc295a0d39f3b9
Adam Schlesinger <adam@freshplanet.com>
2016-05-13 12:19:15 -0400

    formatting

--- android/src/com/freshplanet/inapppurchase/BaseFunction.java
+++ android/src/com/freshplanet/inapppurchase/BaseFunction.java
@@ -8,60 +8,55 @@ import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
 import com.adobe.fre.FREObject;
 
-public class BaseFunction implements FREFunction
-{
+public class BaseFunction implements FREFunction {
+
 	@Override
-	public FREObject call(FREContext context, FREObject[] args)
-	{
+	public FREObject call(FREContext context, FREObject[] args) {
 		return null;
 	}
 	
-	protected String getStringFromFREObject(FREObject object)
-	{
-		try
-		{
+	protected String getStringFromFREObject(FREObject object) {
+
+		try {
 			return object.getAsString();
 		}
-		catch (Exception e)
-		{
+		catch (Exception e) {
+
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
-	protected Boolean getBooleanFromFREObject(FREObject object)
-	{
-		try
-		{
+	protected Boolean getBooleanFromFREObject(FREObject object) {
+
+		try {
 			return object.getAsBool();
 		}
-		catch(Exception e)
-		{
+		catch (Exception e) {
+
 			e.printStackTrace();
 			return false;
 		}
 	}
 	
-	protected List<String> getListOfStringFromFREArray(FREArray array)
-	{
+	protected List<String> getListOfStringFromFREArray(FREArray array) {
+
 		List<String> result = new ArrayList<String>();
 		
-		try
-		{
-			for (int i = 0; i < array.getLength(); i++)
-			{
-				try
-				{
+		try {
+
+            for (int i = 0; i < array.getLength(); i++) {
+
+				try {
 					result.add(getStringFromFREObject(array.getObjectAt((long)i)));
 				} 
-				catch (Exception e)
-				{
+				catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		catch (Exception e)
-		{
+		catch (Exception e) {
+
 			e.printStackTrace();
 			return null;
 		}
